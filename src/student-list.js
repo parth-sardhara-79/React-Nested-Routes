7855acf57931c97f102e27bc55d3b6b580471c32
parth-sardhara-79 <parthsardhara026@gmail.com>
2020-02-07 14:54:33 +0530

    Page not found

--- src/student-list.js
+++ src/student-list.js
@@ -8,50 +8,48 @@ import PageNotFound from './page-not-found';
 class ListStudents extends React.Component {
     render() {
 
-        return (
-            <div className="studentList">
-                <Table  bordered responsive>
-                    <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Standard</th>
-                            <th>Gender</th>
-                            <th colSpan="2" className="btnCenter">Action</th>
-                        </tr>
-                    </thead>
-                    {
-                        data.map(u => {
-                            return (
-                                <tr>
-                                    <td>{u.first_name}</td>
-                                    <td>{u.last_name}</td>
-                                    <td>{u.standard}</td>
-                                    <td>{u.gender}</td>
-                                    <td className="btnCenter">
-                                        <Button id={u.id} variant="outline-dark"
-                                            onClick={() =>
-                                                this.props.history.push(`/students-list/${u.id}/student-details`)
-                                            }>View Details
+        return (<div className="studentList">
+            <Table bordered responsive>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Standard</th>
+                        <th>Gender</th>
+                        <th colSpan="2" className="btnCenter">Action</th>
+                    </tr>
+                </thead>
+                {
+                    data.map(u => {
+                        return (
+                            <tr>
+                                <td>{u.first_name}</td>
+                                <td>{u.last_name}</td>
+                                <td>{u.standard}</td>
+                                <td>{u.gender}</td>
+                                <td className="btnCenter">
+                                    <Button id={u.id} variant="outline-dark"
+                                        onClick={() =>
+                                            this.props.history.push(`/students-list/${u.id}/student-details`)
+                                        }>View Details
                                         </Button>
-                                    </td>
-                                    <td className="btnCenter">
-                                        <Button id={u.id} variant="outline-dark"
-                                            onClick={() =>
-                                                this.props.history.push(`/students-list/${u.id}/student-marks`)
-                                            }>View Marks
+                                </td>
+                                <td className="btnCenter">
+                                    <Button id={u.id} variant="outline-dark"
+                                        onClick={() =>
+                                            this.props.history.push(`/students-list/${u.id}/student-marks`)
+                                        }>View Marks
                                         </Button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </Table>
-                <hr />
-                <Route  exact path="/students-list/:id/student-marks" component={StudentMarks} />
-                <Route  exact path="/students-list/:id/student-details" component={StudentDetails} />
-            </div>);
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </Table>
+            <hr />
+            <Route exact path="/students-list/:id/student-marks" component={StudentMarks} />
+            <Route exact path="/students-list/:id/student-details" component={StudentDetails} />
+        </div>);
     }
 }
-
 export default ListStudents;
